91d3f3751b8d53b60b687a2baca060057392e425
sergiosistemapeoplenet <sergio@sistemapeoplenet.com.br>
2025-08-07 21:49:45 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { CadastroTarefas } from './components/pages/cadastro-tarefas/cadastro-tarefas';
+import { ConsultaTarefas } from './components/pages/consulta-tarefas/consulta-tarefas';
+import { EdicaoTarefas } from './components/pages/edicao-tarefas/edicao-tarefas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'pages/cadastro-tarefas',
+        component: CadastroTarefas
+    },
+    {
+        path : 'pages/consulta-tarefas',
+        component: ConsultaTarefas
+    },
+    {
+        path : 'pages/edicao-tarefas',
+        component: EdicaoTarefas
+    },
+    {
+        path: '', pathMatch: 'full',
+        redirectTo: '/pages/consulta-tarefas'
+    }
+];
